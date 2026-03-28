6fcf6ddff5220921ed4a8bd95a69561e45d75805
Parth <parthvitnor@gmail.com>
2026-03-28 16:14:16 +0530

    feat: add variables for secondary VPC and subnet CIDR blocks

--- variables.tf
+++ variables.tf
@@ -2,14 +2,23 @@ variable "region" {
   default = "ap-south-1" #Mumbai 
 }
 
-variable "vpc_cidr_block" {
+variable "primary_vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
+variable "primary_subnet_cidr_block" {
   default = "10.0.1.0/24"
 }
 
 variable "subnet_availability_zone" {
   default = "ap-south-1a"
+}
+
+
+variable "secondary_vpc_cidr_block" {
+  default = "11.0.0.0/16"
+}
+
+variable "secondary_subnet_cidr_block" {
+  default = "11.0.1.0/24"
 }
