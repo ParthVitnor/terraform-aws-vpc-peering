7029c678a7419ffa2f702911480478f71f9332f5
Parth <parthvitnor@gmail.com>
2026-03-28 13:39:46 +0530

    refactor: rename VPCs from public/private to primary/secondary

--- main.tf
+++ main.tf
@@ -1,96 +1,96 @@
-resource "aws_vpc" "public_vpc" {
+resource "aws_vpc" "primary_vpc" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "public-vpc"
+    Name = "primary-vpc"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.public_vpc.id
+resource "aws_subnet" "primary_subnet" {
+  vpc_id = aws_vpc.primary_vpc.id
   cidr_block = var.subnet_cidr_block
   availability_zone = var.subnet_availability_zone
   map_public_ip_on_launch = true
 
   tags = {
-    Name ="public-subnet"
+    Name ="primary-subnet"
   }
 }
 
-resource "aws_internet_gateway" "public_vpc-igw" {
-  vpc_id = aws_vpc.public_vpc.id
+resource "aws_internet_gateway" "primary_vpc_igw" {
+  vpc_id = aws_vpc.primary_vpc.id
 
   tags = {
-    Name = "public-vpc-igw"
+    Name = "primary-vpc-igw"
   }
 }
 
-resource "aws_route_table" "public_vpc_rt" {
-  vpc_id = aws_vpc.public_vpc.id
+resource "aws_route_table" "primary_vpc_rt" {
+  vpc_id = aws_vpc.primary_vpc.id
 
   tags = {
-    Name = "public-rt"
+    Name = "primary-rt"
   }
 }
 
-resource "aws_route" "public_vpc_igw" {
+resource "aws_route" "route_to_igw" {
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw_public_vpc.id
-  route_table_id = aws_route_table.public_vpc_rt.id
+  gateway_id = aws_internet_gateway.primary_vpc_igw.id
+  route_table_id = aws_route_table.primary_vpc_rt.id
 }
 
 
-resource "aws_route_table_association" "igw_internet_assoc" {
-  subnet_id = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_vpc_rt.id
+resource "aws_route_table_association" "primary_vpc_internet_assoc" {
+  subnet_id = aws_subnet.primary_subnet.id
+  route_table_id = aws_route_table.primary_vpc_rt.id
 }
 
-resource "aws_vpc" "private_vpc" {
-  cidr_block = "11.0.0.0/0"
+resource "aws_vpc" "secondary_vpc" {
+  cidr_block = "11.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "private-vpc"
+    Name = "secondary-vpc"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.private_vpc.id
-  cidr_block = "11.0.1.0/0"
+resource "aws_subnet" "secondary_subnet" {
+  vpc_id = aws_vpc.secondary_vpc.id
+  cidr_block = "11.0.1.0/24"
   availability_zone = var.subnet_availability_zone
 
   tags =  {
-    Name = "private-subnet"
+    Name = "secondary-subnet"
   }
 }
 
 
-resource "aws_internet_gateway" "private-vpc-igw" {
-  vpc_id = aws_vpc.private_vpc.id
+resource "aws_internet_gateway" "secondary-vpc-igw" {
+  vpc_id = aws_vpc.secondary_vpc.id
 
   tags = {
-    Name = "private-vpc-igw"
+    Name = "secondary-vpc-igw"
   }
 }
 
-resource "aws_route_table" "private_vpc_rt" {
-  vpc_id = aws_vpc.private_vpc.id
+resource "aws_route_table" "secondary_vpc_rt" {
+  vpc_id = aws_vpc.secondary_vpc.id
 
   tags = {
-    Name ="private-rt"
+    Name ="secondary-rt"
   }
 }
 
-resource "aws_route" "private_rote" {
+resource "aws_route" "secondary_rote" {
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.private-vpc-igw.id
-  route_table_id = aws_route_table.private_vpc_rt.id
+  gateway_id = aws_internet_gateway.secondary-vpc-igw.id
+  route_table_id = aws_route_table.secondary_vpc_rt.id
 }
 
-resource "aws_route_table_association" "name" {
-  subnet_id = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_vpc_rt.id
+resource "aws_route_table_association" "primary_vpc_internet_assoc" {
+  subnet_id = aws_subnet.secondary_subnet.id
+  route_table_id = aws_route_table.secondary_vpc_rt.id
 }
