c5aa7846f381a7f57f6544d002a1ce18f605f373
Parth <parthvitnor@gmail.com>
2026-03-31 21:02:17 +0530

    feat: add SSH ingress rule for primary instance

--- main.tf
+++ main.tf
@@ -182,3 +182,14 @@ resource "aws_security_group" "primary_sg" {
 }
 
 
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type = "ingress"
+  security_group_id = aws_security_group.primary_sg.id
+  from_port = 22
+  protocol = "tcp"
+  to_port = 22
+  cidr_blocks = "0.0.0.0/0"  
+}
+
+
