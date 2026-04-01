bc155c79b8d6cb565af79190ae1624131ba94e10
Parth <parthvitnor@gmail.com>
2026-04-01 08:12:24 +0530

    feat: add SSH ingress rule for secondary security group

--- main.tf
+++ main.tf
@@ -233,3 +233,12 @@ resource "aws_security_group" "secondary_sg" {
     Name = "secondary_sg"
   }
 }
+
+resource "aws_security_group_rule" "allow_ssh_secondary" {
+  type = "ingress"
+  security_group_id = aws_security_group.secondary_sg.id
+  from_port = 22
+  protocol = "tcp"
+  to_port = 22
+  cidr_blocks = "0.0.0.0/0"  
+}
